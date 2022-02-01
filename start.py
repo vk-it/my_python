b841b4a5730ff210577072bd425a4f06e9c7655b
vk-it <46228289+vk-it@users.noreply.github.com>
2022-02-01 13:57:19 +0300

    watermelon

--- start.py
+++ start.py
@@ -1,4 +1,24 @@
 print('hello')
 print('world')
 print('all')
+print('Проверка')
 
+#for i in 'hello world':
+#    if i == 'o':
+#        continue
+#    print(i *2, end ='')
+print(4+5)
+#import this
+small = False
+green = True
+if small:
+    if green:
+        print("pea")
+    else:
+        print("cherry")
+else:
+    if green:
+#Ты попадаешь в else, потому что small - False, далее там встречается if green, который True и отдает тебе арбуз, \\потому что труе выпало на арбуз
+        print("watermelon")
+    else:
+        print("pumpkin")
